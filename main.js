8fb89b222f4675c7d081897250e3ba133a21cb9e
easparansheshan <easparansheshan456@gmail.com>
2021-02-22 13:40:44 +0530

    all songs updated clearly

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ SC.initialize({
 
 $(document).ready(function () {
 
-    SC.stream('/tracks/966127855', function (sound) {
+    SC.stream('/tracks/51442761', function (sound) {
 
         $('#start').click(function (e) {
             e.preventDefault();
@@ -17,7 +17,7 @@ $(document).ready(function () {
         });
 
     });
-    SC.stream('/tracks/168167974', function (sound) {
+    SC.stream('/tracks/191214159', function (sound) {
 
         $('#start1').click(function (e) {
             e.preventDefault();
@@ -30,7 +30,7 @@ $(document).ready(function () {
         });
 
     });
-    SC.stream('/tracks/191214159', function (sound) {
+    SC.stream('/tracks/556842441', function (sound) {
 
         $('#start2').click(function (e) {
             e.preventDefault();
@@ -61,9 +61,13 @@ $(document).ready(function () {
 
 // aathi song
 //<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/168167974&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/my-songs-10" title="Rahul Rockzzz" target="_blank" style="color: #cccccc; text-decoration: none;">Rahul Rockzzz</a> · <a href="https://soundcloud.com/my-songs-10/12316546sdf546465" title="Aathi-Kaththi(2014)" target="_blank" style="color: #cccccc; text-decoration: none;">Aathi-Kaththi(2014)</a></div>
+//<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/51442761&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/praveena-rajah-1" title="Praveena Rajah 1" target="_blank" style="color: #cccccc; text-decoration: none;">Praveena Rajah 1</a> · <a href="https://soundcloud.com/praveena-rajah-1/sets/kathi-songs" title="kathi songs" target="_blank" style="color: #cccccc; text-decoration: none;">kathi songs</a></div>
 
 //enakena yarum ilaye
 //<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/191214159&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/insanegowtham" title="Gowtham" target="_blank" style="color: #cccccc; text-decoration: none;">Gowtham</a> · <a href="https://soundcloud.com/insanegowtham/enakenna-yarum-ila" title="Enakenna Yaarum Illaye - Aakko Tamil Movie Single | Anirudh Ravichander" target="_blank" style="color: #cccccc; text-decoration: none;">Enakenna Yaarum Illaye - Aakko Tamil Movie Single | Anirudh Ravichander</a></div>
 
 //usure pohuthe song
 //<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/345819962&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/arrsong" title="Ar Rahman Tamil Songs" target="_blank" style="color: #cccccc; text-decoration: none;">Ar Rahman Tamil Songs</a> · <a href="https://soundcloud.com/arrsong/usure-pogudhey-1" title="Usure Pogudhey" target="_blank" style="color: #cccccc; text-decoration: none;">Usure Pogudhey</a></div>
+
+//rowdy baby
+//<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/556842441&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/thepavelstar" title="ThePavelStar" target="_blank" style="color: #cccccc; text-decoration: none;">ThePavelStar</a> · <a href="https://soundcloud.com/thepavelstar/maari-2-rowdy-baby-dhanush-sai-pallavi-yuvan" title="Maari 2 - Rowdy Baby - Dhanush, Sai Pallavi ,Yuvan" target="_blank" style="color: #cccccc; text-decoration: none;">Maari 2 - Rowdy Baby - Dhanush, Sai Pallavi ,Yuvan</a></div>
