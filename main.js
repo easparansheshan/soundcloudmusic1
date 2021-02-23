4fb4e19e98bfae2d344a292b5b91c006df9e2b8b
easparansheshan <easparansheshan456@gmail.com>
2021-02-23 11:20:22 +0530

    i am so lonely

--- main.js
+++ main.js
@@ -70,7 +70,7 @@ $(document).ready(function () {
         });
 
     });
-    SC.stream('/tracks/345819962', function (sound) {
+    SC.stream('/tracks/179383705', function (sound) {
 
         $('#start6').click(function (e) {
             e.preventDefault();
@@ -127,3 +127,6 @@ $(document).ready(function () {
 
 //English i am rider
 //<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/649577930&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/ankit-srdr" title="Ankit Sharma ✪" target="_blank" style="color: #cccccc; text-decoration: none;">Ankit Sharma ✪</a> · <a href="https://soundcloud.com/ankit-srdr/imran-khan-i-am-a-rider-satisfya8d-audio" title="Imran Khan I Am A Rider Satisfya(8D AUDIO)USE HEADPHONES" target="_blank" style="color: #cccccc; text-decoration: none;">Imran Khan I Am A Rider Satisfya(8D AUDIO)USE HEADPHONES</a></div>
+
+// i am so lonely
+//<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/179383705&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/v-giang-xu-n" title="Võ Giang Xuân" target="_blank" style="color: #cccccc; text-decoration: none;">Võ Giang Xuân</a> · <a href="https://soundcloud.com/v-giang-xu-n/i-am-so-lonely-broken-angel-broken-angel" title="I Am So Lonely Broken Angel - Broken Angel" target="_blank" style="color: #cccccc; text-decoration: none;">I Am So Lonely Broken Angel - Broken Angel</a></div>
