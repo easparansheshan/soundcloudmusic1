0073d3ac4e28e73d62599cef4088eb55643b4908
easparansheshan <easparansheshan456@gmail.com>
2021-02-24 19:15:28 +0530

    add

--- main.js
+++ main.js
@@ -217,7 +217,7 @@ $(document).ready(function () {
     });
 
     //carousel songs
-    SC.stream('/playlists/172599801', function (sound) {
+    SC.stream('playlists/692249400', function (sound) {
 
         $('#start17').click(function (e) {
             e.preventDefault();
@@ -332,3 +332,5 @@ $(document).ready(function () {
 
 //drums play
 //<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/121742038&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/djnataliaparis" title="NATALIA PARIS OFICIAL" target="_blank" style="color: #cccccc; text-decoration: none;">NATALIA PARIS OFICIAL</a> · <a href="https://soundcloud.com/djnataliaparis/you-play-my-drums" title="you play my drums" target="_blank" style="color: #cccccc; text-decoration: none;">you play my drums</a></div>
+
+//<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/692249400&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/user-709845406" title="User 709845406" target="_blank" style="color: #cccccc; text-decoration: none;">User 709845406</a> · <a href="https://soundcloud.com/user-709845406/sets/quitar" title="quitar" target="_blank" style="color: #cccccc; text-decoration: none;">quitar</a></div>
